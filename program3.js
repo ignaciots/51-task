7be51e8c968375c3b3a2c556f403775533ea33a2
Nachex <elnano@gmail.com>
2017-04-19 19:19:31 +0200

    Change identation on programs

--- program3.js
+++ program3.js
@@ -1,6 +1,6 @@
-for(var i = 1; i <= 51; i++ ) {
-	if ( i == 13 || i == 23 || i == 45)
-		continue;
-	if ( i % 2 != 0 ) 
-		console.log(i);
+for (var i = 1; i <= 51; i++) {
+    if (i == 13 || i == 23 || i == 45)
+        continue;
+    if (i % 2 != 0)
+        console.log(i);
 }
